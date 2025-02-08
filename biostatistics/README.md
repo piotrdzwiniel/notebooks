52424b3f93ad9715e0eb91cc3e7849ba76918e21
Piotr Dzwiniel <piotr.dzwiniel@gmail.com>
2025-02-08 01:28:34 +0100

    Update.

---
+++ biostatistics/README.md
@@ -0,0 +1,3 @@
+The resources in this folder are designed to help you learn and pursue a career as a
+biostatistician. Here you will find both scripts, notebooks, and articles useful for designing
+experiments, processing and analyzing data.
--- biostatistics/sample_size/SampleSizeForTwoSampleTTest.py
+++ biostatistics/sample_size/SampleSizeForTwoSampleTTest.py
@@ -1,8 +1,8 @@
 from statsmodels.stats.power import TTestIndPower
 
 # Define parameters
-effect_size = 0.5  # Cohen's d (medium effect); small = 0.2, medium = 0.5, large = 0.8
-alpha = 0.05       # Significance level; (5% risk of false positives)
+effect_size = 1.0  # Cohen's d (medium effect); small = 0.2, medium = 0.5, large = 0.8
+alpha = 0.01       # Significance level; (5% risk of false positives)
 power = 0.8        # Power (1 - β); requires 80% power to detect the effect
 ratio = 1.0        # Assumes equal group sizes
 
